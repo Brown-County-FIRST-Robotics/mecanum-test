4777bcea0d12a12cad33472ebed98f36f5e54996
Colin <cfinn68916@gmail.com>
2022-10-19 17:01:27 -0500

    add init function stuff

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # mecanum-test
+This is a combination of the launcher.py and robot.py files in the auto_aim branch of last year's code. 
--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -1,6 +1,6 @@
-/*
-TODO: add rumble
-TODO: add autonomous functions
+/*  
+	TODO:
+	move motor varibles to struct
 */
 #include <iostream>
 #include <string>
@@ -8,7 +8,6 @@ TODO: add autonomous functions
 #include "frc/TimedRobot.h"
 #include "frc/Joystick.h"
 #include "frc/Solenoid.h"
-#include "frc/smartdashboard/SmartDashboard.h"
 #include "ctre/Phoenix.h"
 #include "frc/motorcontrol/MotorControllerGroup.h"
 #include "frc/drive/MecanumDrive.h"
@@ -18,6 +17,7 @@ TODO: add autonomous functions
 #include "frc/GenericHID.h"
 #include "units/time.h"
 #include "frc/XboxController.h"
+#include "frc/AnalogInput.h"
 
 using namespace frc;
 
@@ -25,30 +25,74 @@ class Robot : public TimedRobot
 {
 public:
 	WPI_TalonFX back_left{0};
+	double back_left_target_pos=0;
+	double back_left_pos=0;
+	double back_left_previous_pos=0;
 	WPI_TalonFX front_left{1};
+	double front_left_target_pos=0;
+	double front_left_pos=0;
+	double front_left_previous_pos=0;
 	WPI_TalonFX front_right{2};
+	double front_right_target_pos=0;
+	double front_right_pos=0;
+	double front_right_previous_pos=0;
 	WPI_TalonFX back_right{3};
+	double back_right_target_pos=0;
+	double back_right_pos=0;
+	double back_right_previous_pos=0;
 	WPI_TalonFX shooter_top{6};
 	WPI_TalonFX shooter_bottom{7};
-	WPI_TalonSRX shooter_angle_1{11};
-	WPI_TalonSRX shooter_angle_2{10};
+	WPI_TalonSRX shooter_angle_1{10};
+	WPI_TalonSRX shooter_angle_2{11};
 	WPI_TalonFX intake{12};
+	double intake_target_pos=0;
+	double intake_pos=0;
+	double intake_previous_pos=0;
 
 	DoubleSolenoid shooter_solenoid{PneumaticsModuleType::CTREPCM, 0, 1};
 	Timer shooter_solenoid_timer;
-	int shooter_solenoid_previous_position = shooter_solenoid.kReverse;
 
 	DoubleSolenoid intake_solenoid{PneumaticsModuleType::CTREPCM, 2, 3};
+	int shooter_solenoid_previous_position = shooter_solenoid.kForward;
 	Timer intake_solenoid_timer;
 
+	DoubleSolenoid climber_solenoid{PneumaticsModuleType::CTREPCM, 4, 5};
+	int climber_solenoid_previous_position = shooter_solenoid.kReverse;
+	Timer climber_solenoid_timer;
+
 	MotorControllerGroup shooter_angle{shooter_angle_2, shooter_angle_1};
+	double shooter_angle_target_pos=0;
+	double shooter_angle_pos=0;
+	double shooter_angle_previous_pos=0;
+	MotorControllerGroup shooter{shooter_bottom, shooter_top};
+	double shooter_target_pos=0;
+	double shooter_pos=0;
+	double shooter_previous_pos=0;
+
+	
 
 	XboxController controller{0};
 
 	GenericHID hid{0};
+	
 	float driveSpeed = 0.5;
+	float shooter_angle_speed = 1;
+	float shooter_speed = .35;
+	bool is_birb_activated = true;
+	bool hub_shooting[2] = {true,true}; // [isHubShooting, isMovingToCorrectAngle]
+	Timer hub_shooting_timer;
+	AnalogInput potentiometer{0};
+
+	Timer timer;
+	Timer shooter_timer;
 
 	MecanumDrive mecDrive{front_left, back_left, front_right, back_right};
+
+	bool is_shooting=false;
+	double shooter_step=0;
+	bool lock_on_mode=false;
+	Timer shooting_timer;
+	bool firing=false;
 
 	void SimulationPeriodic()
 	{
@@ -60,7 +104,7 @@ public:
 		double joyY = -controller.GetLeftY();
 		double joyR = controller.GetRightX();
 
-		/* deadband gamepad 5%*/
+		/* deadband gamepad 5% */
 		if (fabs(joyR) < 0.05)
 			joyR = 0;
 		if (fabs(joyX) < 0.05)
@@ -68,85 +112,20 @@ public:
 		if (fabs(joyY) < 0.05)
 			joyY = 0;
 		mecDrive.DriveCartesian(joyY, joyX, joyR);
-		if (controller.GetPOV() == -1 || controller.GetPOV() == 90 || controller.GetPOV() == 270)
-		{
-			shooter_angle.Set(0);
-		}
-		else if (controller.GetPOV() == 0)
-		{
-			shooter_angle.Set(-.5);
-		}
-		else if (controller.GetPOV() == 180)
-		{
-			shooter_angle.Set(.5);
-		}
 
-		double rTrigger = controller.GetRightTriggerAxis();
-		double lTrigger = controller.GetLeftTriggerAxis();
-
-		if (rTrigger > lTrigger)
-		{
-			intake.Set(rTrigger);
-			shooter_bottom.Set(rTrigger);
-			shooter_top.Set(rTrigger);
-		}
-		if (rTrigger < lTrigger)
-		{
-			intake.Set(lTrigger * -.8);
-			shooter_bottom.Set(lTrigger * -.8);
-			shooter_top.Set(lTrigger * -.8);
-		}
-
-		if (rTrigger == lTrigger)
-		{
-			intake.Set(0);
-			shooter_bottom.Set(0);
-			shooter_top.Set(0);
-		}
-
-		/*if(rTrigger!=0){
-			RUMBLE
-		}else{
-			DO NOT RUMBLE
-		}*/
-
-		// solenoids:
-		if (controller.GetAButton())
-		{
-			shooter_solenoid_timer.Start();
-			if (shooter_solenoid_previous_position == DoubleSolenoid::kForward)
-			{
-				shooter_solenoid.Set(DoubleSolenoid::kReverse);
-				shooter_solenoid_previous_position = DoubleSolenoid::kReverse;
-			}
-			if (shooter_solenoid_previous_position == DoubleSolenoid::kReverse)
-			{
-				shooter_solenoid.Set(DoubleSolenoid::kForward);
-				shooter_solenoid_previous_position = DoubleSolenoid::kForward;
-			}
-		}
-		units::second_t quarterOfASecond = .25_s;
-		if (shooter_solenoid_timer.HasElapsed(quarterOfASecond))
-		{
-			shooter_solenoid_timer.Stop();
-			shooter_solenoid_timer.Reset();
-			shooter_solenoid_previous_position = shooter_solenoid.Get();
-			shooter_solenoid.Set(DoubleSolenoid::kOff);
-		}
 	}
 
 	void RobotInit()
 	{
-		/* factory default values */
-		front_right.ConfigFactoryDefault();
-		back_right.ConfigFactoryDefault();
-		front_left.ConfigFactoryDefault();
-		back_left.ConfigFactoryDefault();
+		timer.Start();
+		front_left.SetNeutralMode(Brake);
+		back_left.SetNeutralMode(Brake);
+		front_right.SetNeutralMode(Brake);
+		back_right.SetNeutralMode(Brake);
+		shooter_top.SetInverted(true);
+		shooter_angle_1.SetInverted(true);
+		shooter_angle_2.SetInverted(true);
 
-		front_right.SetInverted(TalonFXInvertType::Clockwise);
-		back_right.SetInverted(TalonFXInvertType::FollowMaster);
-		front_left.SetInverted(TalonFXInvertType::CounterClockwise);
-		back_left.SetInverted(TalonFXInvertType::FollowMaster);
 	}
 
 private:
